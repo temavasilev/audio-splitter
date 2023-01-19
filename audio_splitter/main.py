a08e38f2415660cfcf145381175cc796b9a209ab
Artem Vasilev <artem.vasilev@rwth-aachen.de>
2023-01-19 11:18:24 +0100

    enabled the ability to split multiple audio files

--- audio_splitter/main.py
+++ audio_splitter/main.py
@@ -6,18 +6,21 @@ import os
 
 def main():
     if args.audio:
-        try:
-            audio = pydub.AudioSegment.from_file(args.audio)
-            file_name = os.path.basename(args.audio).split(".")[0]
-            print(audio)
-        except FileNotFoundError:
-            print("File not found!")
-            return
-        split_audio(audio, file_name)
-        exit(0)
-    elif args.all != "/data":
+        for file in args.audio:
+            print(f"Processing {file}")
+            try:
+                audio = pydub.AudioSegment.from_file(file)
+                file_name = os.path.basename(file).split(".")[0]
+            except FileNotFoundError:
+                print(f"{file} not found!")
+                continue
+            except pydub.exceptions.CouldntDecodeError:
+                print(f"{file} is not an audio file!")
+                continue
+            split_audio(audio, file_name)
+    if args.input:
         files = [f for f in os.listdir(args.all) if f.endswith(".mp3")]
-        print(f"Founds {len(list(files))} files, named {files}")
+        print(f"Found {len(list(files))} files, named {files}")
         for file in files:
             print(f"Processing {args.all}/{file}")
             audio = pydub.AudioSegment.from_file(f"{args.all}/{file}")
@@ -27,27 +30,34 @@ def main():
     
 def split_audio(audio, name: str):
     number_of_chunks = get_chunk_number(audio)
-    export_path = pathlib.Path(f"{args.output}") if args.output else pathlib.Path("/data")
+    export_path = pathlib.Path(f"{args.output}") if args.output else pathlib.Path(os.getcwd())
     for i in range(number_of_chunks):
         chunk = audio[i * len(audio) // number_of_chunks:(i + 1) * len(audio) // number_of_chunks]
         chunk.export(f"{export_path}/{name}_{i+1:02d}.mp3", format="mp3")
 
-def get_chunk_number(audio) -> int:
+def get_chunk_number(audio: pydub.AudioSegment) -> int:
+    if args.number:
+        print(f"Splitting audio into {args.number} chunks with a duration of {audio.duration_seconds // args.number // 60} minutes")
+        return args.number
     if audio.duration_seconds < 1800: # pointless to split short audio into chunks
         print("Audio is too short")
         return 1
     else:
         number_of_chunks = int(audio.duration_seconds // 1800 + 1)
-        print("Splitting audio into {} chunks with a duration of 30 minutes".format(number_of_chunks))
+        print(f"Splitting audio into {number_of_chunks} chunks with a duration of {audio.duration_seconds // number_of_chunks // 60} minutes")
         return number_of_chunks
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("audio", help="Path to audio file", nargs="?")
-    parser.add_argument("-a", "--all", help="Split all audio files in the directory", type=pathlib.Path, default="/data")
+    parser.add_argument("audio", help="Path to audio file", nargs="*")
+    parser.add_argument("-i", "--input", help="Split all audio files in the directory", type=pathlib.Path)
     parser.add_argument("-d", "--directory", help="Path to directory")
+    parser.add_argument("-n", "--number", help="Number of chunks", type=int)
     parser.add_argument("-o", "--output", help="Path to output directory")
     args = parser.parse_args()
+    if args.number and args.number < 1:
+        print("Number of chunks must be greater than 0")
+        exit(1)
     main()
 
