9909e9ed625b00f497aa876abd1eea9d3625b877
Artem Vasilev <temavasilev@proton.me>
2023-03-30 19:50:34 +0200

    Added split on silence

---
+++ README.md
@@ -0,0 +1,45 @@
+# Audio File Splitter
+
+`audio_splitter/main.py` is a command-line tool for splitting audio files into equally-sized chunks or based on silence. This Python script uses the `pydub` library for handling various audio formats.
+
+## Why does this exist?
+
+As someone who enjoys to listen to spoken word every night, I often listen to podcasts on my old MP3 player before going to sleep. However, I found myself growing bored of listening to the same first 5 minutes of each episode every night. To mix things up and always have something new to listen to, I created this script to break down podcast episodes into smaller chunks. Now I can jump around different parts of the episodes and enjoy a fresh listening experience each time.
+
+## Usage
+
+To use the script, follow these steps:
+
+```bash
+python audio_splitter/main.py input_file output_dir [--chunk_length CHUNK_LENGTH] [--output_format OUTPUT_FORMAT] [--silence_based]
+```
+
+Replace input_file with the path to your audio file and output_dir with the path to the output directory where you want to save the chunks.
+
+## Options
+```bash
+--chunk_length CHUNK_LENGTH: Length of each chunk in milliseconds (default: 300000 ms, or 5 minutes). Ignored if --silence_based is set.
+--output_format OUTPUT_FORMAT: Output format for the audio chunks (default: wav). Supported formats include wav, mp3, and ogg.
+--silence_based: Split the audio based on silence instead of fixed-size chunks. If set, --chunk_length is ignored and the script ensures that the generated chunks are at least the specified length.
+```
+
+## Examples
+
+Split an audio file into 5-minute (default) chunks:
+```bash
+python audio_splitter/main.py input_file.mp3 output_dir
+```
+
+Split an audio file into 10-minute chunks and save them in the MP3 format:
+```bash
+python audio_splitter/main.py input_file.mp3 output_dir --chunk_length 600000 --output_format mp3
+```
+
+Split an audio file based on silence, ensuring that the chunks are at least 5 minutes long:
+```python audio_splitter/main.py input_file.mp3 output_dir --silence_based
+```
+
+Split an audio file based on silence, ensuring that the chunks are at least 10 minutes long:
+```bash
+python audio_splitter/main.py input_file.mp3 output_dir --chunk_length 600000 --silence_based
+```
--- audio_splitter/main.py
+++ audio_splitter/main.py
@@ -1,64 +1,93 @@
-import pydub
 import argparse
-import pathlib
 import os
+from concurrent.futures import ThreadPoolExecutor
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from tqdm import tqdm
 
+def save_chunk(chunk, start_time, output_dir, output_format):
+    chunk.export(os.path.join(output_dir, f'chunk_{start_time}.{output_format}'), format=output_format)
+
+def merge_short_chunks(chunks, min_chunk_length_ms):
+    merged_chunks = []
+    current_chunk = chunks[0]
+
+    for chunk in chunks[1:]:
+        if len(current_chunk) + len(chunk) < min_chunk_length_ms:
+            current_chunk += chunk
+        else:
+            merged_chunks.append(current_chunk)
+            current_chunk = chunk
+
+    merged_chunks.append(current_chunk)
+    return merged_chunks
+
+def split_audio(input_file, output_dir, chunk_length_ms, output_format, silence_based):
+    # Load the input audio file using Pydub
+    audio = AudioSegment.from_file(input_file)
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if silence_based:
+        # Split the audio file based on silence
+        min_silence_len = 100  # Minimum length of silence in milliseconds
+        silence_thresh = -40   # Silence threshold in dB
+        chunks = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
+
+        # Merge adjacent chunks shorter than the specified length
+        chunks = merge_short_chunks(chunks, chunk_length_ms)
+
+        # Set up progress bar with tqdm
+        pbar = tqdm(total=len(chunks), desc="Processing chunks based on silence")
+
+        # Save chunks in parallel using ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            for i, chunk in enumerate(chunks):
+                executor.submit(save_chunk, chunk, i, output_dir, output_format).add_done_callback(lambda x: pbar.update(1))
+
+    else:
+        # Calculate the total length of the audio in milliseconds and the number of full chunks
+        audio_length_ms = len(audio)
+        num_chunks = audio_length_ms // chunk_length_ms
+
+        # Set up progress bar with tqdm
+        pbar = tqdm(total=num_chunks + (audio_length_ms % chunk_length_ms != 0), desc="Processing fixed-size chunks")
+
+        # Split and save chunks in parallel using ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            for i in range(num_chunks):
+                start_time = i * chunk_length_ms
+                end_time = (i + 1) * chunk_length_ms
+                chunk = audio[start_time:end_time]
+                executor.submit(save_chunk, chunk, start_time, output_dir, output_format).add_done_callback(lambda x: pbar.update(1))
+
+            # Handle the last chunk if there is any remainder
+            if audio_length_ms % chunk_length_ms != 0:
+                start_time = num_chunks * chunk_length_ms
+                end_time = audio_length_ms
+                chunk = audio[start_time:end_time]
+                executor.submit(save_chunk, chunk, start_time, output_dir, output_format).add_done_callback(lambda x: pbar.update(1))
+
+    # Close progress bar
+    pbar.close()
+    
 
 def main():
-    if args.audio:
-        for file in args.audio:
-            print(f"Processing {file}")
-            try:
-                audio = pydub.AudioSegment.from_file(file)
-                file_name = os.path.basename(file).split(".")[0]
-            except FileNotFoundError:
-                print(f"{file} not found!")
-                continue
-            except pydub.exceptions.CouldntDecodeError:
-                print(f"{file} is not an audio file!")
-                continue
-            split_audio(audio, file_name)
-    if args.input:
-        files = [f for f in os.listdir(args.input) if f.endswith(".mp3")]
-        print(f"Found {len(list(files))} files, named {files}")
-        for file in files:
-            print(f"Processing {args.input}/{file}")
-            audio = pydub.AudioSegment.from_file(f"{args.input}/{file}")
-            file_name = os.path.basename(file).split(".")[0]
-            split_audio(audio, file_name)
-        exit(0)
-    
-def split_audio(audio, name: str):
-    number_of_chunks = get_chunk_number(audio)
-    audio = audio.set_channels(1)
-    export_path = pathlib.Path(f"{args.output}") if args.output else pathlib.Path(os.getcwd())
-    for i in range(number_of_chunks):
-        chunk = audio[i * len(audio) // number_of_chunks:(i + 1) * len(audio) // number_of_chunks]
-        chunk.export(f"{export_path}/{name}_{i+1:02d}.mp3", format="mp3", bitrate="32k")
+    # Set up argument parser for the CLI app
+    parser = argparse.ArgumentParser(description="Split an audio file into equally sized chunks.")
+    parser.add_argument("input_file", help="Path to the input audio file.")
+    parser.add_argument("output_dir", help="Path to the output directory where chunks will be saved.")
+    parser.add_argument("--chunk_length", type=int, default=300000, help="Length of each chunk in milliseconds (default: 300000 ms / 5 minutes).")
+    parser.add_argument("--output_format", type=str, default="wav", help="Output format for the audio chunks (default: wav). Supported formats include wav, mp3, and ogg.")
+    parser.add_argument("--silence_based", action="store_true", help="Split the audio based on silence instead of fixed-size chunks. If set, --chunk_length is ignored.")
 
-def get_chunk_number(audio: pydub.AudioSegment) -> int:
-    if args.number:
-        print(f"Splitting audio into {args.number} chunks with a duration of {audio.duration_seconds // args.number // 60} minutes")
-        return args.number
-    if audio.duration_seconds < 1800: # pointless to split short audio into chunks
-        print("Audio is too short")
-        return 1
-    else:
-        number_of_chunks = int(audio.duration_seconds // 1800 + 1)
-        print(f"Splitting audio into {number_of_chunks} chunks with a duration of {audio.duration_seconds // number_of_chunks // 60} minutes")
-        return number_of_chunks
+    # Parse the arguments
+    args = parser.parse_args()
 
+    # Call the split_audio function with the provided arguments
+    split_audio(args.input_file, args.output_dir, args.chunk_length, args.output_format, args.silence_based)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("audio", help="Path to audio file", nargs="*")
-    parser.add_argument("-i", "--input", help="Split all audio files in the directory", type=pathlib.Path)
-    parser.add_argument("-d", "--directory", help="Path to directory")
-    parser.add_argument("-n", "--number", help="Number of chunks", type=int)
-    parser.add_argument("-o", "--output", help="Path to output directory")
-    args = parser.parse_args()
-    if args.number and args.number < 1:
-        print("Number of chunks must be greater than 0")
-        exit(1)
     main()
-
